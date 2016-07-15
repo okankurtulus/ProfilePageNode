2fb846ae71b6afe809fb6609d7cbbd3e9ba09667
Okan Kurtulus <okankurtulus@gmail.com>
2016-07-15 11:48:36 +0300

    - Created first api

---
+++ api/controllers/HistoryController.js
@@ -0,0 +1,11 @@
+/**
+ * HistoryController
+ *
+ * @description :: Server-side logic for managing Histories
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
